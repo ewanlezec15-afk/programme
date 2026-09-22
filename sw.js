7ee2806dd6660519184b805089e7cb0c280a098c
ewanlezec15-afk <ewanlezec15@gmail.com>
2026-09-22 02:19:38 +0200

    Retrait de la date

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "programme-v1";
+const CACHE = "programme-v2";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
